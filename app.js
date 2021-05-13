6d883332d7419cce3373df54618dbfe1b685e492
Nyein Chan Aung <ninja.aung02@gmail.com>
2021-05-13 04:35:12 -0700

    Added disconnection event

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(3000, () => {
-  console.log('listening on localhost:3000');
+    console.log('listening on localhost:3000');
 });
